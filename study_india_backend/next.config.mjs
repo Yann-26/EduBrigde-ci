f187f4dd7b113462f010ddcfc6b32dc5b0686c06
Yann-26 <yannassiri26@gmail.com>
2026-05-01 16:01:35 +0530

    first release1

--- study_india_backend/next.config.mjs
+++ study_india_backend/next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: 'http://localhost:5173' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS' },
           { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization' },
         ],
