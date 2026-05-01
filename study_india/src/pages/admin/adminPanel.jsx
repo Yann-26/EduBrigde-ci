b4c3b32c29b185b18892751113ba2cbd3aef7031
Yann-26 <yannassiri26@gmail.com>
2026-05-01 23:48:11 +0530

    .....1

--- study_india/src/pages/admin/adminPanel.jsx
+++ study_india/src/pages/admin/adminPanel.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import Sidebar from '../../components/admin/Sidebar'
+import Sidebar from '../../components/admin/SideBar'
 import Header from '../../components/admin/Header'
 import DashBoard from './Dashboard'
 import Applications from './Applications'
